a60e3304c167ff5966a5dada23902eb004b3e712
Artur Neri <arturnerictt@gmail.com>
2024-04-18 20:04:57 -0300

    absolute path correction

--- app/hamburguer.js
+++ app/hamburguer.js
@@ -5,11 +5,11 @@ const controls = document.querySelector('.header-controls');
 
 // IMAGES
 
-const hamburguer_open_desktop = '../assets/hamburguer-open.png';
-const hamburguer_closed_desktop = '../assets/hamburguer.png';
+const hamburguer_open_desktop = '/assets/hamburguer-open.png';
+const hamburguer_closed_desktop = '/assets/hamburguer.png';
 
-const hamburguer_open_mobile = '../assets/hamburguer-mobile-open.png';
-const hamburguer_closed_mobile = '../assets/hamburguer-mobile.png';
+const hamburguer_open_mobile = '/assets/hamburguer-mobile-open.png';
+const hamburguer_closed_mobile = '/assets/hamburguer-mobile.png';
 
 hamburguer.onclick = handleSideBar;
 
@@ -23,17 +23,17 @@ function handleSideBar() {
 function changeHamburguerImage(vw) {
   if (vw < 468) {
     if (sidebar.classList.contains('hide')){
-      hamburguer.style.backgroundImage = "url('../assets/hamburguer-mobile.png')";
+      hamburguer.style.backgroundImage = "url('/assets/hamburguer-mobile.png')";
       console.log('mobile - closed');
     } else {
-      hamburguer.style.backgroundImage = "url('../assets/hamburguer-mobile-open.png')";
+      hamburguer.style.backgroundImage = "url('/assets/hamburguer-mobile-open.png')";
     }
   } else {
     if (sidebar.classList.contains('hide')){
-      hamburguer.style.backgroundImage = "url('../assets/hamburguer.png')";
+      hamburguer.style.backgroundImage = "url('/assets/hamburguer.png')";
       console.log('dekstop - closed');
     } else {
-      hamburguer.style.backgroundImage = "url('../assets/hamburguer-open.png')";
+      hamburguer.style.backgroundImage = "url('/assets/hamburguer-open.png')";
     }
   }
 }
